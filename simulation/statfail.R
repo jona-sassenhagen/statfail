313e77e77dcd7aef0c69b738581dff7f6a85d1fc
Phillip Alday <phillip.alday@unisa.edu.au>
2016-05-04 18:41:57 +0930

    (hopefully) working data-simulation engine in place

--- simulation/statfail.R
+++ simulation/statfail.R
@@ -22,14 +22,58 @@
 library(reshape2)
 library(plyr)
 
+generate.correlated.data <- function(rho,data,seed.data=NULL){
+
+  # based on http://stats.stackexchange.com/q/15011/26743
+  theta <- acos(rho)  # angle corresponding to correlation
+  x1    <- data       # fixed given data
+  n     <- length(x1)  # number of data points
+  if(is.null(seed.data)){
+    seed.data <- rnorm(n,mean=0,sd=1)
+  }
+  # avoid numerical issues with perfect correlation
+  if(theta == 0){
+    x <- data + mean(seed.data)
+    if(rho < 0){
+      return (-x)
+    }else{
+      return (x)
+    }
+  }
+
+  x2    <- seed.data      # new random data
+  X     <- cbind(x1, x2)         # matrix
+  Xctr  <- scale(X, center=TRUE, scale=FALSE)   # centered columns (mean 0)
+
+  Id   <- diag(n)                               # identity matrix
+  Q    <- qr.Q(qr(Xctr[ , 1, drop=FALSE]))      # QR-decomposition, just matrix Q
+  P    <- tcrossprod(Q)          # = Q Q'       # projection onto space defined by x1
+  x2o  <- (Id-P) %*% Xctr[ , 2]                 # x2ctr made orthogonal to x1ctr
+  Xc2  <- cbind(Xctr[ , 1], x2o)                # bind to matrix
+  Y    <- Xc2 %*% diag(1/sqrt(colSums(Xc2^2)))  # scale columns to length 1
+
+  x <- Y[ , 2] + (1 / tan(theta)) * Y[ , 1]     # final new vector
+
+  x
+}
+
 simulate <- function(manipulation.effect.size, confound.feature.size,
                      confound.feature.effect.correlation, n.items){
 
-  control <- rnorm(n.items,mean=0)
+  # this is the manipulation itself -- two normally distributed groups with sd=1
+  # and difference in means given by Cohen's d (whose denominator is the sd is 1)
   manipulation <- control + rnorm(n.items,mean=manipulation.effect.size)
+  control <- rnorm(n.items,mean=0)
 
+  # this is the feature as measured, which is *not* the same as the impact of that
+  # feature on the outcome variable
   control.feature <- rnorm(n.items,0)
   manipulation.feature <- rnorm(n.items, mean=confound.feature.size)
+
+  # this is impact of the feature on the outcome variable, computed by producing a dataset with the same
+  # feature-outcome.impact correlation
+  control.confound <- generate.correlated.data(rho=confound.feature.effect.correlation, data=control.feature)
+  manipulation.confound <-  generate.correlated.data(rho=confound.feature.effect.correlation,data=manipulation.feature)
 
 
   outcomes <- data.frame(item=1:n.items,
@@ -39,29 +83,38 @@ simulate <- function(manipulation.effect.size, confound.feature.size,
                         control=control.feature,
                         manipulation=manipulation.feature)
 
+  confounds <- data.frame(item=1:n.items,
+                         control=control.confound,
+                         manipulation=manipulation.confound)
+
   outcomes <- melt(outcomes,id.vars="item",variable.name="condition",value.name="outcome")
   features <- melt(features,id.vars="item",variable.name="condition",value.name="feature")
-
-  #matrix(cbind(features[[2]],features[[2]]))
-  # based on http://stats.stackexchange.com/a/15035/26743
-  confound <- rnorm(n.items,mean=confound.feature.size)
-  X <- cbind(confound.feature,confound)
-
-  C <- matrix(confound.feature.effect.correlation, nrow = 2, ncol = 2)
-  diag(C) <- 1
-  C <- chol(C)
-  XC <-  X %*% C
+  confounds <- melt(confounds,id.vars="item",variable.name="condition",value.name="confound")
 
   results <- join(outcomes,features,by=c("item","condition"))
+  results <- join(results,confounds,by=c("item","condition"))
+
+  results$confounded.outcome <- results$outcome + results$confound
 
   results
 }
 
-x <- simulate(manipulation.effect.size=2,confound.effect.size=1,confound.feature.size=1,n.items=20)
-summary(lm(manipulation ~ control, data=x))
+# x <- simulate(manipulation.effect.size=2,confound.feature.size=1,confound.feature.effect.correlation=0.8,n.items=20)
+# summary(lm(outcome ~ condition, data=x))
+# t.test(outcome~condition,data=x)
+# t.test(confounded.outcome~condition,data=x)
+# t.test(feature~condition,data=x)
 
+resimulate <- function(n,...){
+  # strip out the iteration number being passed via lapply
+  lambda <- function(x,...) simulate(...)
 
+  x <- lapply(1:n,lambda,...)
+  result <- do.call(rbind,x)
 
-resimulate <- function(...,n){
+  result$iter <- sort(rep(seq(n),nrow(result)/n))
 
+  result
 }
+
+#resimulate(n=100,manipulation.effect.size=2,confound.feature.size=1,confound.feature.effect.correlation=0.8,n.items=20)
